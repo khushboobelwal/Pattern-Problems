77d1974033c9928c818e8a28e45eca60decd21c2
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-15 12:04:25 +0530

    Update LeftPyramidPattern.java

--- LeftPyramidPattern.java
+++ LeftPyramidPattern.java
@@ -1,1 +1,17 @@
-
+class Solutions {
+static   void printLeftPyramid(int n) {
+	        for(int line=1;line<=n;line++){
+	            for(int star=1;star<=line;star++){
+	                
+	                    System.out.print("*");
+	                
+	            }
+	            System.out.println();
+	        }
+	        // code here
+	    }
+	  public static void main(String args[]) {
+		  int n=5;
+		  printLeftPyramid(n);
+	  }
+	}
