5ebf04ec0984a30fba50468ba701716d0aa7e9f6
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-15 11:54:02 +0530

    Update HollowSquarePattern.java

--- HollowSquarePattern.java
+++ HollowSquarePattern.java
@@ -1,1 +1,20 @@
-
+class Solutions {
+	  static   void printSquare(int n) {
+	        for(int line=1;line<=n;line++){
+	            for(int star=1;star<=n;star++){
+	                if(line==1||line==n||star==1||star==n){
+	                    System.out.print("*");
+	                }
+	                else{
+	                    System.out.print(" ");
+	                }
+	            }
+	            System.out.println();
+	        }
+	        // code here
+	    }
+	  public static void main(String args[]) {
+		  int n=4;
+		  printSquare(n);
+	  }
+	}
